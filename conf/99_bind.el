0f271b2473ef2fecd02f4d730f82871761ca011a
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2017-06-01 12:17:38 +0900

    fix

--- conf/99_bind.el
+++ conf/99_bind.el
@@ -337,15 +337,15 @@ _t_: regenerate tag    _s_: switch
 
 (defhydra ~hydra-projectile-rails-find-misc (:exit t :hint nil)
   "
-_g_: find gemfile      _m_: find migration    _M_: find migration in other project    _C-m_: find current migration
-_r_: find routes       _e_: find environment  _E_: find environment in other project
-_d_: find schema       _s_: find serializer   _S_: find serializer in other project   _C-s_: find current serializer
-_D_: find seeds        _l_: find lib          _L_: find lib in other project
-_h_: find spec helper  _f_: find feature      _F_: find feature in other project
-                       _o_: find log          _O_: find log in other project
-                       _y_: find layout       _Y_: find layout in other project
-                       _t_: find rake task    _T_: find rake task in other project
-                       _v_: find validator    _V_: find validator in other project
+_m_: find migration    _M_: find migration in other project    _C-m_: find current migration   _g_: find gemfile
+_e_: find environment  _E_: find environment in other project                                  _r_: find routes
+_s_: find serializer   _S_: find serializer in other project   _C-s_: find current serializer  _d_: find schema
+_l_: find lib          _L_: find lib in other project                                          _D_: find seeds
+_f_: find feature      _F_: find feature in other project                                      _h_: find spec helper
+_o_: find log          _O_: find log in other project
+_y_: find layout       _Y_: find layout in other project
+_t_: find rake task    _T_: find rake task in other project
+_v_: find validator    _V_: find validator in other project
 "
   ("m"   projectile-rails-find-migration)
   ("M"   projectile-rails-find-migration--in-other-project)

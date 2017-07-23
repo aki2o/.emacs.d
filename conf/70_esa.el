c1042497eec95055e4a5a3c4374e170401d7b2de
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2017-07-23 10:50:21 +0900

    use esa-direx

--- conf/70_esa.el
+++ conf/70_esa.el
@@ -1,7 +1,13 @@
-;; (bundle esa)
-;; (use-package esa
-;;   :init
-;;   (setq esa-token (~auth-source-get-property 'token :app "esa.mf"))
-;;   (setq esa-team-name "mf")
-;;   )
+(bundle aki2o/emacs-esa-cui :name esa-cui)
+(bundle aki2o/esa-direx)
+(use-package esa-direx
+  :defer t
+  
+  :config
+
+  (esa-cui:regist-team "mf"
+                       :access-token (~auth-source-get-property 'token :app "esa.mf"))
+  
+  (esa-cui--log-enable-logging)
+  (esa-direx--log-enable-logging))
 

f9bdb34561faff4a6032af502e40f940c61c0f33
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2018-12-25 09:55:29 +0900

    update yasnippet config

--- conf/11_yasnippet.el
+++ conf/11_yasnippet.el
@@ -89,7 +89,7 @@
     (yas-choose-value (symbol-value (intern (format "~yas-%s" sym)))))
 
   (defvar ~yas-rspec-matchers
-    '("eq " "eql " "equal " "include " "cover " "match " "start_with " "end_with "
+    '("eq " "eql " "equal " "include " "cover " "match " "start_with " "end_with " "contain_exactly()"
       "be " "be_a_kind_of " "be_a " "be_truthy" "be_falsey" "be_nil" "be_present" "be_empty" "be_added()"
       "be > " "be >= " "be < " "be <= " "be_"
       "have_been_enqueued" "have_"

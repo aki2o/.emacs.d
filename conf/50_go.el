a4906e7999d65636ead47568bfcbf6d2b58e058f
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2020-03-23 06:32:58 +0900

    go のコード補完に lsp-mode を使うことにする

--- conf/50_go.el
+++ conf/50_go.el
@@ -6,14 +6,11 @@
 (bundle go-autocomplete)
 (bundle company-go)
 (use-package go-mode
-  :init
-
-  (setq gofmt-command "goimports") ; go get golang.org/x/tools/cmd/goimports
-
+  :custom (gofmt-command "goimports") ; go get golang.org/x/tools/cmd/goimports
   :config
-
   (defun ~go-setup-mode ()
-    (add-hook 'before-save-hook 'gofmt-before-save t t))
+    (add-hook 'before-save-hook 'gofmt-before-save t t)
+    (setq-local tab-width 2))
   
   (add-hook 'go-mode-hook '~go-setup-mode t)
 
@@ -33,18 +30,9 @@
     (defun ~go-setup-color-moccur ()
       (setq moccur-grep-default-mask (mmask-get-regexp-string 'go-mode)))
     (add-hook 'go-mode-hook '~go-setup-color-moccur t))
-  
-  (use-package go-autocomplete
-    :init
-    (setq ac-go-expand-arguments-into-snippets nil))
 
-  (use-package go-eldoc
-    :init
-    (add-hook 'go-mode-hook 'go-eldoc-setup t))
-
-  (use-package go-direx
-    :init
-    (define-key go-mode-map (kbd "C-c C-j") 'go-direx-pop-to-buffer))
+  (use-package lsp-mode
+    :hook (go-mode . lsp))
 
   (use-package go-projectile
     :config

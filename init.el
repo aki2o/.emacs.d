99e1f865482631a1dc80fbf59546975b72611e23
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2020-03-23 06:34:14 +0900

    自動更新

--- init.el
+++ init.el
@@ -73,6 +73,6 @@
  '(mozc-leim-title "[あ]")
  '(package-selected-packages
    (quote
-    (go-rename helm-rg rg vue-mode dash-functional git-commit ivy org oauth2 gitignore-mode github-browse-file csv-mode company)))
+    (company-box go-rename helm-rg rg vue-mode dash-functional git-commit ivy org oauth2 gitignore-mode github-browse-file csv-mode company)))
  '(rspec-use-rake-when-possible nil)
  '(terraform-indent-level 2))

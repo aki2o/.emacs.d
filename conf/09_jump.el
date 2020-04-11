b70ef61ca08b1f3bda64201fe3e295ae64e8c223
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2020-04-12 01:55:45 +0900

    goto-chg には undo-tree が必要らしい

--- conf/09_jump.el
+++ conf/09_jump.el
@@ -3,11 +3,10 @@
   :commands (point-undo point-redo))
 
 
-(bundle goto-chg)
+(bundle goto-chg :depends (undo-tree))
 (use-package goto-chg
   :commands (goto-last-change goto-last-change-reverse))
 
 
 (~tags-be-find-tag-command find-function)
 (~tags-be-find-tag-command find-variable)
-

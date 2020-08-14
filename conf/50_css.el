cdfa57af4b0e4f4805e97347a5a635449674e949
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2020-08-14 23:40:35 +0900

    Update css config

--- conf/50_css.el
+++ conf/50_css.el
@@ -15,8 +15,13 @@
   (add-hook 'css-mode-hook '~css-mode-setup t)
   (defun ~css-mode-setup ()
     (set (make-local-variable 'css-indent-offset) 2)
+    (setq ~tidy-code-current-function '~css-tidy)
     ;; color-moccur
     (setq moccur-grep-default-mask (mmask-get-regexp-string 'css-mode))))
+
+(defun ~css-tidy ()
+  (interactive)
+  (~dockerize-shell-command (format "$(npm bin)/stylelint --fix %s" (shell-quote-argument (~projectile-relative-path (current-buffer))))))
 
 
 (use-package scss-mode
@@ -26,5 +31,6 @@
   (defun ~scss-mode-setup ()
     (set (make-local-variable 'css-indent-offset) 2)
     (set (make-local-variable 'scss-compile-at-save) nil)
+    (setq ~tidy-code-current-function '~css-tidy)
     ;; color-moccur
     (setq moccur-grep-default-mask (mmask-get-regexp-string 'scss-mode))))

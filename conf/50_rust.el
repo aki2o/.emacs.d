992dfa328e8c870ebba4ad75ec696db5c1a96080
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2021-11-22 01:14:27 +0900

    Update rust config

--- conf/50_rust.el
+++ conf/50_rust.el
@@ -1,12 +1,13 @@
 (use-package rust-mode
   :defer t
-  :custom (rust-format-on-save t)
+  :custom ((rust-format-on-save t)
+           (rust-indent-offset 2))
   :hook (rust-mode . ~rust-setup))
 
 (defun ~rust-setup ()
   (when (fboundp 'lsp)
-    ;; (setq lsp-rust-server 'rust-analyzer)
-    ;; (setq lsp-rust-analyzer-server-command '("rust-analyzer"))
+    (setq lsp-rust-server 'rust-analyzer)
+    (setq lsp-rust-analyzer-server-command '("rust-analyzer"))
     (lsp)))
 
 (use-package cargo

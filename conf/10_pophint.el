c3ec6e6ec28b1e51030ec30f7ef5e2e2f9b557c2
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2022-10-22 00:38:20 +0900

    [wip] Setup

--- conf/10_pophint.el
+++ conf/10_pophint.el
@@ -1,91 +1,88 @@
-(bundle pophint :type git :url "git@github.com:aki2o/emacs-pophint.git" :branch "refactor_for_emacs_28")
-(use-package pophint
-  :defer t
-  :custom ((pophint:popup-chars "hjklyuiopnmgfdatre;")
-           (pophint:popup-max-tips 1000)
-           (pophint:switch-direction-p nil)
-           (pophint:select-source-method 'use-popup-char)
-           ;; (pophint:select-source-chars "tregfdbvc")
-           (pophint:switch-source-selectors '(("Quoted"   . "q")
-                                              ("Url/Path" . "u")
-                                              ("Cmt"      . "c")
-                                              ("Line"     . "l")
-                                              ("Sym"      . "s")))
-           (pophint-mark:yank-immediately-on-marking-p nil)
-           (pophint-isearch:start-on-isearch-exit-p nil)
-           (pophint-yank:relayout-on-start-rangeyank-p nil)
-           (pophint-eww:use-new-tab t)
-           (pophint-w3m:use-new-tab t)
-           (pophint-e2wm:array-quit-immediately t))
+(bundle pophint :type git :url "git@github.com:aki2o/emacs-pophint.git" :branch "refactor_for_emacs_28"
+  (make-directory-autoloads default-directory "pophint-autoloads.el"))
+(require 'pophint-autoloads)
+(setq pophint:popup-chars "hjklyuiopnmgfdatre;")
+(setq pophint:popup-max-tips 1000)
+(setq pophint:switch-direction-p nil)
+(setq pophint:select-source-method 'use-popup-char)
+;; (pophint:select-source-chars "tregfdbvc")
+(setq pophint:switch-source-selectors '(("Quoted"   . "q")
+                                        ("Url/Path" . "u")
+                                        ("Cmt"      . "c")
+                                        ("Line"     . "l")
+                                        ("Sym"      . "s")))
+(setq pophint-mark:yank-immediately-on-marking-p nil)
+(setq pophint-isearch:start-on-isearch-exit-p nil)
+(setq pophint-yank:relayout-on-start-rangeyank-p nil)
+(setq pophint-eww:use-new-tab t)
+(setq pophint-w3m:use-new-tab t)
+(setq pophint-e2wm:array-quit-immediately t)
   
-  :init
-  (custom-set-faces
-   '(pophint:tip-face     ((t (:background "HotPink4" :foreground "white" :bold t))))
-   '(pophint:match-face   ((t (:background "dark slate gray" :foreground "white"))))
-   '(pophint:pos-tip-face ((t (:background "black" :foreground "white")))))
+(custom-set-faces
+ '(pophint:tip-face     ((t (:background "HotPink4" :foreground "white" :bold t))))
+ '(pophint:match-face   ((t (:background "dark slate gray" :foreground "white"))))
+ '(pophint:pos-tip-face ((t (:background "black" :foreground "white")))))
 
-  :config
-  (pophint:set-allwindow-command pophint:do-flexibly)
-  (pophint:set-allwindow-command pophint:do-rangeyank)
+(pophint:set-allwindow-command pophint:do-flexibly)
+(pophint:set-allwindow-command pophint:do-rangeyank)
 
-  ;; (bind-keys* ("M-y"   . pophint:do-flexibly-yank)
-  ;;             ("C-M-y" . pophint:do-rangeyank)
-  ;;             ("C-M-h" . pophint-region:backward)
-  ;;             ("C-M-l" . pophint-region:forward)
-  ;;             ("H-f"   . pophint-region:delete)
-  ;;             ("H-d"   . pophint-region:backward-delete))
-  ;;             ("C-H-f" . pophint-region:kill)
-  ;;             ("C-H-d" . pophint-region:backward-kill))
+;; (bind-keys* ("M-y"   . pophint:do-flexibly-yank)
+;;             ("C-M-y" . pophint:do-rangeyank)
+;;             ("C-M-h" . pophint-region:backward)
+;;             ("C-M-l" . pophint-region:forward)
+;;             ("H-f"   . pophint-region:delete)
+;;             ("H-d"   . pophint-region:backward-delete))
+;;             ("C-H-f" . pophint-region:kill)
+;;             ("C-H-d" . pophint-region:backward-kill))
 
-  (define-key dired-mode-map (kbd ";") 'pophint:do-dired-node)
+(define-key dired-mode-map (kbd ";") 'pophint:do-dired-node)
 
-  (add-hook 'Info-mode-hook
-            '(lambda () (local-set-key (kbd ";") 'pophint:do-info-ref))
-            t)
+(add-hook 'Info-mode-hook
+          '(lambda () (local-set-key (kbd ";") 'pophint:do-info-ref))
+          t)
 
-  (add-hook 'help-mode-hook
-            '(lambda () (local-set-key (kbd ";") 'pophint:do-help-btn))
-            t)
+(add-hook 'help-mode-hook
+          '(lambda () (local-set-key (kbd ";") 'pophint:do-help-btn))
+          t)
 
-  (add-hook 'Custom-mode-hook
-            '(lambda () (local-set-key (kbd ";") 'pophint:do-widget))
-            t)
+(add-hook 'Custom-mode-hook
+          '(lambda () (local-set-key (kbd ";") 'pophint:do-widget))
+          t)
 
-  (pophint-tags:advice-command find-tag)
-  (pophint-isearch:replace-to-yank-region isearch-yank-line)
-  (pophint-isearch:replace-to-yank-region migemo-isearch-yank-line)
+;; (with-eval-after-load 'eww
+;;   (define-key eww-mode-map (kbd ";") 'pophint:do-eww-anchor))
 
-  (pophint-thing:advice-thing-at-point-function ~dwim-thing-at-point)
+;; For p-r
 
-  (pophint-tags:advice-command ~xref-find-references)
-  (pophint-tags:advice-command ~xref-find-definitions)
+(defun popup-delete (popup)
+  "Delete POPUP instance."
+  (when (popup-live-p popup)
+    (popup-hide popup)
+    (mapc 'delete-overlay (popup-overlays popup))
+    (setf (popup-overlays popup) nil)
+    (setq popup-instances (delq popup popup-instances))
+    ;; Restore newlines state
+    (let ((newlines (popup-newlines popup)))
+      (when (> newlines 0)
+        (popup-save-buffer-state
+         (goto-char (point-max))
+         (dotimes (i newlines)
+           (if (and (char-before)
+                    (= (char-before) ?\n))
+               (delete-char -1)))))))
+  nil)
 
-  (pophint-thing:advice-thing-at-point-function ~browse-search-initial-input)
-  (pophint-thing:defcommand-noadvice ~browse-search)
-  (pophint-thing:defcommand-noadvice ~browse-search-internally)
+(pophint-isearch:replace-to-yank-region isearch-yank-line)
+(pophint-isearch:replace-to-yank-region migemo-isearch-yank-line)
 
-  ;; (with-eval-after-load 'eww
-  ;;   (define-key eww-mode-map (kbd ";") 'pophint:do-eww-anchor))
+(pophint-tags:advice-command ~find-tag-elisp)
+(pophint-tags:advice-command ~xref-find-references)
+(pophint-tags:advice-command ~xref-find-definitions)
 
-  ;; For p-r
-
-  (defun popup-delete (popup)
-    "Delete POPUP instance."
-    (when (popup-live-p popup)
-      (popup-hide popup)
-      (mapc 'delete-overlay (popup-overlays popup))
-      (setf (popup-overlays popup) nil)
-      (setq popup-instances (delq popup popup-instances))
-      ;; Restore newlines state
-      (let ((newlines (popup-newlines popup)))
-        (when (> newlines 0)
-          (popup-save-buffer-state
-            (goto-char (point-max))
-            (dotimes (i newlines)
-              (if (and (char-before)
-                       (= (char-before) ?\n))
-                  (delete-char -1)))))))
-    nil))
+(pophint-thing:advice-thing-at-point-function ~dwim-thing-at-point)
+(pophint-thing:advice-thing-at-point-function ~browse-search-initial-input)
+(pophint-thing:defcommand-noadvice ~browse-search)
+(pophint-thing:defcommand-noadvice ~browse-search-internally)
 
 (defun ~pophint:forward ()
   (interactive)

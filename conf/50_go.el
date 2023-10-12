6d3b3c8a4b050af162e52732d6d1b4cd0eeefc07
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2023-10-12 23:56:12 +0900

    Update go about lsp

--- conf/50_go.el
+++ conf/50_go.el
@@ -3,18 +3,18 @@
   :defer t
   :config
   (~add-setup-hook 'go-mode
-    (setq-local tab-width 2))
+    (setq-local tab-width 2)
+
+    (when (functionp 'lsp)
+      (add-hook 'before-save-hook 'lsp-format-buffer t t)
+      (add-hook 'before-save-hook 'lsp-organize-imports t t)
+      (lsp-deferred)))
 
   (~add-setup-hook-after-load 'mmask 'go-mode
     (setq moccur-grep-default-mask (mmask-get-regexp-string 'go-mode)))
 
-  (when (functionp 'lsp)
+  (with-eval-after-load 'lsp-mode
     (lsp-register-custom-settings '(("gopls.completeUnimported" t t))))
-
-  (~add-setup-hook-after-load 'lsp-mode 'go-mode
-    (add-hook 'before-save-hook 'lsp-format-buffer t t)
-    (add-hook 'before-save-hook 'lsp-organize-imports t t)
-    (lsp-deferred))
   )
 
 

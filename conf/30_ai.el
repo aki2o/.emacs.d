344ca0a4b22752e07f28965c91a1e1c024e181a4
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2023-10-13 16:02:18 +0900

    Add copilot

---
+++ conf/30_ai.el
@@ -0,0 +1,17 @@
+(bundle copilot :type github :pkgname "zerolfx/copilot.el" :branch "main")
+(use-package copilot
+  :defer t
+  :custom ((copilot-idle-delay 5))
+  :hook ((prog-mode . copilot-mode))
+
+  :config
+  (bind-keys :map copilot-completion-map
+             ("C-<return>" . copilot-accept-completion)
+             ("C-," . copilot-previous-completion)
+             ("C-." . copilot-next-completion)
+             ("C-z" . copilot-clear-overlay))
+
+  (bind-keys :map ~keyjack-mode-map
+             ("C-M-SPC" . copilot-complete))
+
+  (add-to-list 'copilot-major-mode-alist '("enh-ruby" . "ruby")))

cb796752e8c330ba8aa34606da90b1bcc2a39cf5
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2023-10-15 16:19:17 +0900

    Update view config

--- conf/05_view.el
+++ conf/05_view.el
@@ -1,36 +1,13 @@
-(use-package view
-  :defer t
-  :config
+(with-eval-after-load 'view
   (setq view-read-only t)
 
-  (defvar ~view-next-activate nil)
-  (defun ~view-toggle-next-activate ()
-    (interactive)
-    (message "View mode be active automatically : %s"
-             (setq ~view-next-activate (not ~view-next-activate))))
+  (bind-keys :map view-mode-map
+             ("C-j"   . next-line)
+             ("C-k"   . previous-line)
+             ("C-S-j" . ~view-scroll-up)
+             ("C-S-k" . ~view-scroll-down))
 
-  (add-hook 'find-file-hook '~view-chk-next-activate t)
-  (defun ~view-chk-next-activate ()
-    (when (and ~view-next-activate
-               (buffer-file-name))
-      (read-only-mode 1)))
-
-  (defun ~view-scroll-up ()
-    (interactive)
-    (View-scroll-line-forward 10))
-
-  (defun ~view-scroll-down ()
-    (interactive)
-    (View-scroll-line-backward 10))
-
-  (defvar ~view-mode-shortcut-keys
-    '("C-h" "C-j" "C-k" "C-l" "C-a" "C-e" "C-{" "C-}" "C-<" "C->"
-      "C-S-h" "C-S-j" "C-S-k" "C-S-l" "C-S-a" "C-S-e"
-      "C-s" "C-S-s" "C-p"))
-
-  (add-hook 'view-mode-hook '~view-mode-setup t)
-  (defvar ~view-mode-bind-customize-finish (make-hash-table))
-  (defun ~view-mode-setup ()
+  (~add-setup-hook 'view-mode
     (when (not (gethash major-mode ~view-mode-bind-customize-finish))
       (with-temp-buffer
         (cl-loop for key in ~view-mode-shortcut-keys
@@ -42,37 +19,46 @@
                  do (define-key view-mode-map (kbd (or shift-char char)) cmd)))
       (puthash major-mode t ~view-mode-bind-customize-finish)))
 
-  (bind-keys :map view-mode-map
-             ("C-j"   . next-line)
-             ("C-k"   . previous-line)
-             ("C-S-j" . ~view-scroll-up)
-             ("C-S-k" . ~view-scroll-down)))
+  (add-hook 'find-file-hook '~view-chk-next-activate t))
+
+(defvar ~view-mode-bind-customize-finish (make-hash-table))
+
+(defvar ~view-mode-shortcut-keys
+  '("C-h" "C-j" "C-k" "C-l" "C-a" "C-e" "C-{" "C-}" "C-<" "C->"
+    "C-S-h" "C-S-j" "C-S-k" "C-S-l" "C-S-a" "C-S-e"
+    "C-s" "C-S-s" "C-p"))
+
+(defvar ~view-next-activate nil)
+
+(defun ~view-scroll-up ()
+  (interactive)
+  (View-scroll-line-forward 10))
+
+(defun ~view-scroll-down ()
+  (interactive)
+  (View-scroll-line-backward 10))
+
+(defun ~view-toggle-next-activate ()
+  (interactive)
+  (message "View mode be active automatically : %s"
+           (setq ~view-next-activate (not ~view-next-activate))))
+
+(defun ~view-chk-next-activate ()
+  (when (and ~view-next-activate
+             (buffer-file-name))
+    (read-only-mode 1)))
 
 
 (bundle viewer :type github :pkgname "rubikitch/viewer")
 (use-package viewer
-  :defer t
+  :after (view)
+  :custom ((viewer-modeline-color-unwritable "indian red")
+           (viewer-modeline-color-view "hot pink")
+           (view-mode-by-default-regexp (rx-to-string
+                                         `(or (and "." (or "log" "gz") eos)
+                                              (and "/gems/")
+                                              (and bos ,(expand-file-name (concat user-emacs-directory "elisp/")))))))
   :config
-  ;; view-modeのときはモードラインの色を変える
-  (setq viewer-modeline-color-unwritable "indian red")
-  (setq viewer-modeline-color-view "hot pink")
   (viewer-change-modeline-color-setup)
-
-  ;; view-modeで開くファイル
-  (setq view-mode-by-default-regexp
-        (rx-to-string `(or (and "." (or "log" "gz") eos)
-                           (and "/.rbenv/")
-                           ;; (and bos ,(expand-file-name (concat user-emacs-directory "elisp/")))
-                           (and "/vendor/bundle/"))))
-  
-  ;; 書き込めないファイルは常にview-mode
-  (viewer-stay-in-setup)
-
-  ;; view-modeを抜けても戻らなくなってしまったのでコメントアウト
-  ;; ;; キーバインドをモード間で統一
-  ;; (define-overriding-view-mode-map emacs-lisp-mode ("RET" . find-function-at-point))
-  ;; (define-overriding-view-mode-map c-mode ("RET" . gtags-find-tag-from-here))
-
-  (defadvice package-menu-execute (around ~disable-view-setting activate)
-    (let ((view-mode-by-default-regexp nil))
-      ad-do-it)))
+  (viewer-stay-in-setup) ;; 書き込めないファイルは常にview-mode
+  )

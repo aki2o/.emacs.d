d78fc69d8e562bb8f2c11f1320d9626a51e89a21
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2023-10-20 23:30:29 +0900

    Add css config

---
+++ conf/50_css.el
@@ -0,0 +1,30 @@
+(use-package css-mode
+  :defer t
+  :custom ((cssm-indent-function 'cssm-c-style-indenter))
+
+  :config
+  ;;(mmm-add-classes
+  ;; '((embedded-css
+  ;;    :submode css-mode
+  ;;    :front "<style[^>]*>"
+  ;;    :back "</style>")))
+  ;;
+  ;;(mmm-add-mode-ext-class 'nxml-mode nil 'embedded-css)
+
+  (~add-setup-hook 'css-mode
+    (setq-local css-indent-offset 2))
+
+  (~add-setup-hook-after-load 'mmask 'css-mode
+    (setq moccur-grep-default-mask (mmask-get-regexp-string 'css-mode))))
+
+
+(bundle scss-mode)
+(use-package scss-mode
+  :defer t
+  :config
+  (~add-setup-hook 'scss-mode
+    (setq-local css-indent-offset 2)
+    (setq-local scss-compile-at-save nil))
+
+  (~add-setup-hook-after-load 'mmask 'scss-mode
+    (setq moccur-grep-default-mask (mmask-get-regexp-string 'scss-mode))))

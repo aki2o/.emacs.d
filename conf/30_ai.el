9d7e3bb68cd370c59285194f3908168084c8670f
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2023-10-21 00:05:25 +0900

    Use chatgpt

--- conf/30_ai.el
+++ conf/30_ai.el
@@ -16,3 +16,25 @@
              ("C-M-SPC" . copilot-complete))
 
   (add-to-list 'copilot-major-mode-alist '("enh-ruby" . "ruby")))
+
+
+(bundle magit-popup :type github :pkgname "magit/magit-popup" :branch "master")
+(bundle tablist :type github :pkgname "politza/tablist" :branch "master")
+(bundle tblui :type github :pkgname "Yuki-Inoue/tblui.el" :branch "master")
+(bundle openai :type github :pkgname "emacs-openai/openai" :branch "master")
+(use-package openai
+  :defer t
+  :init
+  (with-eval-after-load 'exec-path-from-shell
+    (exec-path-from-shell-copy-envs '("OPENAI_API_KEY")))
+  :config
+  (setq openai-key (getenv "OPENAI_API_KEY")))
+
+
+(bundle chatgpt :type github :pkgname "emacs-openai/chatgpt" :branch "master")
+(use-package chatgpt
+  :defer t
+  :custom ((chatgpt-max-tokens 5000)
+           (chatgpt-max-history 100)
+           (chatgpt-input-method 'minibuffer)
+           (chatgpt-display-tokens-info t)))

be73ab8a1090b59370b7837ff8beb7cc9b1ddefd
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2023-11-26 21:15:26 +0900

    Fix 09_jump for borg

--- conf/09_jump.el
+++ conf/09_jump.el
@@ -1,17 +1,14 @@
 (setq xref-after-jump-hook '(~pulse-momentary))
 
-(bundle point-undo)
 (use-package point-undo
   :defer t
   :commands (point-undo point-redo))
 
 
-(bundle undo-tree)
 (use-package undo-tree
   :defer t)
 
 
-(bundle goto-chg)
 (use-package goto-chg
   :defer t
   :commands (goto-last-change goto-last-change-reverse))
@@ -20,4 +17,3 @@
 (when (fboundp '~tags-be-find-tag-command)
   (~tags-be-find-tag-command find-function)
   (~tags-be-find-tag-command find-variable))
-

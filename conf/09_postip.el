1daab89f2905760fd2915f955df2c9a8efbb8228
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2024-04-14 14:16:04 +0900

    Update bufloat config

--- conf/09_postip.el
+++ conf/09_postip.el
@@ -3,3 +3,10 @@
   :init
   (setq pos-tip-foreground-color "white")
   (setq pos-tip-background-color "black"))
+
+
+(use-package bufloat
+  :defer t
+  :config
+  (bind-keys :map bufloat-frame-mode-map
+             ("C-c <up>" . bufloat-cancel)))

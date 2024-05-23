e77e2c3d7f56e2b9948275ba82b24f011d461cbf
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2024-05-23 23:51:39 +0900

    Fix pair config removing my:electric-pair-dwim-setup

--- conf/09_pair.el
+++ conf/09_pair.el
@@ -13,44 +13,19 @@
 (add-to-list 'electric-pair-pairs '(?( . ?)))
 (make-variable-buffer-local 'electric-pair-pairs)
 
-(advice-add 'electric-pair-post-self-insert-function :around #'my:electric-pair-dwim-setup)
 (advice-add 'electric-pair--insert :around #'my:electric-pair-dwim)
 
-(defvar my:electric-pair-dwim-done-p nil)
-(defvar my:electric-pair-dwim-last-arg nil)
-(defvar my:electric-pair-dwim-working-p nil)
-
-(defun my:electric-pair-dwim-setup (orig &rest args)
-  (apply orig args)
-  ;; electric-pair--insert からも呼ばれるので、その場合はスキップ
-  (when (not my:electric-pair-dwim-working-p)
-    (when (not my:electric-pair-dwim-done-p)
-      (setq my:electric-pair-dwim-last-arg nil))
-    (setq my:electric-pair-dwim-done-p nil)))
-
 (defun my:electric-pair-dwim (orig &rest args)
-  (let ((arg (nth 0 args)))
-    (cond ((and (eq last-command 'self-insert-command)
-                (eq arg my:electric-pair-dwim-last-arg))
-           ;; 続けて入力した場合は、前回入力を取り消す
-           (setq my:electric-pair-dwim-last-arg nil)
-           (backward-delete-char 1)
-           (and (re-search-forward (format "%c" arg) nil t)
-                (backward-delete-char 1)))
-          (t
-           (let* ((char (char-after))
-                  (pair (assoc-default char electric-pair-pairs))
-                  (my:electric-pair-dwim-working-p t))
-             (when (not (region-active-p))
-               (cond (pair
-                      ;; ペア開始文字上だったら、そのペア終了の後に挿入する
-                      (forward-sexp))
-                     ((and char
-                           (string-match (rx word) (format "%c" char)))
-                      ;; 単語文字上だったら、その後に挿入する
-                      (or (re-search-forward (rx word-end) (pos-eol) t)
-                          (re-search-forward (rx line-end) nil t)
-                          (goto-char (point-max))))))
-             (setq my:electric-pair-dwim-done-p t)
-             (setq my:electric-pair-dwim-last-arg arg)
-             (apply orig args))))))
+  (let* ((char (char-after))
+         (pair (assoc-default char electric-pair-pairs)))
+    (when (not (region-active-p))
+      (cond (pair
+             ;; ペア開始文字上だったら、そのペア終了の後に挿入する
+             (forward-sexp))
+            ((and char
+                  (string-match (rx word) (format "%c" char)))
+             ;; 単語文字上だったら、その後に挿入する
+             (or (re-search-forward (rx word-end) (pos-eol) t)
+                 (re-search-forward (rx line-end) nil t)
+                 (goto-char (point-max))))))
+    (apply orig args)))

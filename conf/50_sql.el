8d772f20c5dd8c8c75da60e30ee01d5ee11e70bb
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2024-10-08 14:48:33 +0900

    Fix mode option for sqlx

--- conf/50_sql.el
+++ conf/50_sql.el
@@ -1,6 +1,7 @@
 (use-package sql-indent
   :after (sql)
   :custom ((sql-indent-offset 2))
+  :mode (("\\.sqlx\\'" . sql-mode))
   :config
   (sql-set-product "mysql") ; mysql, postgres, oracle, sqlite...
   )

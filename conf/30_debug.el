7b14df348997de46e7378e06501817ffb189239a
Hiroaki Otsu <ootsuhiroaki@gmail.com>
2025-05-10 22:39:57 +0900

    [wip] Config dap

--- conf/30_debug.el
+++ conf/30_debug.el
@@ -1,2 +1,25 @@
 (use-package dap-mode
-  :defer t)
+  :defer t
+  :config
+  ;; (setq dap-print-io t)
+  )
+
+;; (require 'dap-node)
+
+;; (defun my:dap-typescript ()
+;;   (interactive)
+;;   (let ((root (projectile-project-root)))
+;;     (dap-debug
+;;      (list :type "node"
+;;            :request "launch"
+;;            :name "typescript"
+;;            :outDir "dist"
+;;            :trace t
+;;            ;; :program (concat root "node_modules/.bin/jest")
+;;            :program (expand-file-name (buffer-file-name (current-buffer)))
+;;            ;; :args (list (~projectile-relative-path (current-buffer)))
+;;            :cwd root
+;;            :runtimeExecutable "pnpm"
+;;            :runtimeArgs (list "test")
+;;            :protocol "inspector"
+;;            :console "integratedTerminal"))))
